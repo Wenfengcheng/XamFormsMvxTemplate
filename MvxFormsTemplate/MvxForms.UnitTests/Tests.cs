dcad988c54e4ed7c985cdff5ed824daa525dc658
Paul Datsiuk <jthotbox@gmail.com>
2017-06-15 13:25:54 +0300

    update vsix project

---
+++ MvxFormsTemplate/MvxForms.UnitTests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace $safeprojectname$
+{
+    public class Tests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
