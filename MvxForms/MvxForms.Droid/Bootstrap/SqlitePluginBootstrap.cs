2869bb10be67dc358cf36cec4e748730ed8a5401
Paul Datsiuk <jthotbox@gmail.com>
2017-07-19 17:55:17 +0300

    update nuget delete sqlite references

--- MvxForms/MvxForms.Droid/Bootstrap/SqlitePluginBootstrap.cs
+++
@@ -1,8 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace MvxForms.Droid.Bootstrap
-{
-    public class SqlitePluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Sqlite.PluginLoader>
-		{}
-}
